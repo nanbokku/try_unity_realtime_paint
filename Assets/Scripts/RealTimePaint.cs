77429667d0c054a6889840a8e5129190feb946ba
nanbokku <29055086+nanbokku@users.noreply.github.com>
2019-11-29 20:45:43 +0900

    [add] Add RealTimePaint scripts

---
+++ Assets/Scripts/RealTimePaint.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealTimePaint : MonoBehaviour
+{
+    [SerializeField]
+    private Material paintMaterial = null;
+    private RenderTexture paintTexture = null;
+    private int paintWorldPositionId = 0;
+    private int viewProjectionInverseId = 0;
+
+    void Awake()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Texture mainTexture = renderer.material.mainTexture;
+
+        // RenderTextureの生成
+        paintTexture = new RenderTexture(mainTexture.width, mainTexture.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Default);
+        paintTexture.enableRandomWrite = true;
+
+        // 元々のテクスチャを生成したRenderTextureにコピー
+        Graphics.Blit(mainTexture, paintTexture);
+
+        // paintMaterialのテクスチャに設定
+        int mainTextureId = Shader.PropertyToID("_MainTex");
+        paintMaterial.SetTexture(mainTextureId, paintTexture);
+
+        // MainTextureを生成したRenderTextureに変更する
+        renderer.material.mainTexture = paintTexture;
+
+        // 各シェーダープロパティIDを取得
+        paintWorldPositionId = Shader.PropertyToID("_PaintWorldPosition");
+        viewProjectionInverseId = Shader.PropertyToID("_ViewProjectionInverse");
+
+        // シェーダーでdepthを取り出すための設定
+        Camera.main.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    void Update()
+    {
+        // Paint();
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        paintMaterial.SetVector(paintWorldPositionId, new Vector4(0, 0, 0, 0));
+
+        // from https://gamedev.stackexchange.com/questions/131978/shader-reconstructing-position-from-depth-in-vr-through-projection-matrix/140924#140924
+        Matrix4x4 projectionMatrix = GL.GetGPUProjectionMatrix(Camera.main.projectionMatrix, false);
+        projectionMatrix[2, 3] = projectionMatrix[3, 2] = 0.0f;
+        projectionMatrix[3, 3] = 1.0f;
+
+        Matrix4x4 clipToWorld = Matrix4x4.Inverse(projectionMatrix * Camera.main.worldToCameraMatrix) * Matrix4x4.TRS(new Vector3(0, 0, -projectionMatrix[2, 2]), Quaternion.identity, Vector3.one);
+        paintMaterial.SetMatrix(viewProjectionInverseId, clipToWorld);
+
+        // ペイント処理
+        Graphics.Blit(src, dest, paintMaterial);
+    }
+
+    void Paint()
+    {
+        paintMaterial.SetVector(paintWorldPositionId, new Vector4(0, 0, 0, 0));
+
+        // from https://gamedev.stackexchange.com/questions/131978/shader-reconstructing-position-from-depth-in-vr-through-projection-matrix/140924#140924
+        Matrix4x4 projectionMatrix = GL.GetGPUProjectionMatrix(Camera.main.projectionMatrix, false);
+        projectionMatrix[2, 3] = projectionMatrix[3, 2] = 0.0f;
+        projectionMatrix[3, 3] = 1.0f;
+
+        Matrix4x4 clipToWorld = Matrix4x4.Inverse(projectionMatrix * Camera.main.worldToCameraMatrix) * Matrix4x4.TRS(new Vector3(0, 0, -projectionMatrix[2, 2]), Quaternion.identity, Vector3.one);
+        paintMaterial.SetMatrix(viewProjectionInverseId, clipToWorld);
+
+        // ペイント処理
+        RenderTexture tmp = RenderTexture.GetTemporary(paintTexture.width, paintTexture.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Default);  // 一時的なテクスチャを作成する
+        Graphics.Blit(paintTexture, tmp, paintMaterial);  // paintMaterialを基にtmpに描画する
+        Graphics.Blit(tmp, paintTexture); // その後，tmpに描画された結果をpaintTextureにコピー
+        RenderTexture.ReleaseTemporary(tmp);  // tmpを解放
+    }
+}
---
+++ Assets/Shaders/RealTimePaint.shader
@@ -0,0 +1,97 @@
+﻿Shader "Custom/RealTimePaint"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}   // ペイント用テクスチャ
+        _PaintColor ("PaintColor", Color) = (0,0,0,0)   // ペイントする色
+        _BrushRadius ("BrushRadius", Float) = 5 // ペイントする範囲（半径）
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+                float4 worldPos : TEXCOORD2;
+                float3 worldDirection : TEXCOORD3;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            fixed4 _PaintColor;
+            float _BrushRadius;
+            float4 _PaintWorldPosition; // ペイント中心位置
+            sampler2D _CameraDepthTexture;
+            float4 _CameraDepthTexture_ST;
+            float4x4 _ViewProjectionInverse; // (projction * view).inverse
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                // o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+
+                // TODO: ワールド座標を求める
+
+                // float4 clip = float4((o.uv.x) * 2 - 1, (o.uv.y) * 2 - 1, 0, 1);
+                float4 clip = float4(o.vertex.xy, 0, 1);
+                o.worldDirection = mul(_ViewProjectionInverse, clip).xyz - _WorldSpaceCameraPos;
+
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+                depth = LinearEyeDepth(depth);
+
+                // float4 H = float4((i.uv.x) * 2 - 1, (i.uv.y) * 2 - 1, depth, 1);
+                float4 H = float4((i.uv.x) * 2 - 1, (i.uv.y) * 2 - 1, 0, 1);
+
+                float4 D = mul(_ViewProjectionInverse, H);
+                D = D / D.w;
+                
+                float3 worldPos = D.xyz + _WorldSpaceCameraPos;
+                // return float4(i.worldDirection, 1);
+                return fixed4(depth, depth, depth, 1);
+                return fixed4(i.worldDirection * depth + _WorldSpaceCameraPos, 1);
+
+                float2 uv = TRANSFORM_TEX(i.uv, _MainTex);
+                fixed4 col = tex2D(_MainTex, uv);
+
+                if (distance(worldPos.xyz, _PaintWorldPosition.xyz) < _BrushRadius) {
+                    col = _PaintColor * tex2D(_MainTex, uv);
+                }
+
+                // // sample the texture
+                // fixed4 col = tex2D(_MainTex, i.uv);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
