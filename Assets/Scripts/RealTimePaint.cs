5ef52a50ade3335a4e639ca1d5036ab4c3024785
nanbokku <29055086+nanbokku@users.noreply.github.com>
2020-02-02 17:38:17 +0900

    :fire: Remove unused assets and files

--- Assets/Scripts/RealTimePaint.cs
+++ Assets/Scripts/RealTimePaint.cs
@@ -1,79 +1,125 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class RealTimePaint : MonoBehaviour
 {
     [SerializeField]
-    private Material paintMaterial = null;
-    private RenderTexture paintTexture = null;
+    private Renderer myRenderer = null;
     private int paintWorldPositionId = 0;
-    private int viewProjectionInverseId = 0;
+    private int prevTexId = 0;
+    private int currentTexId = 0;
+    /// <summary>
+    /// 前フレームのテクスチャを参照するためのテクスチャ．
+    /// paintBuffer2と書き込み，読み込み役を毎フレーム交代する．
+    /// </summary>
+    private RenderTexture paintBuffer1 = null;
+    /// <summary>
+    /// 前フレームのテクスチャを参照するためのテクスチャ．
+    /// paintBuffer1と書き込み，読み込み役を毎フレーム交代する．
+    /// </summary>
+    private RenderTexture paintBuffer2 = null;
+    /// <summary>
+    /// 偶数フレームで描画されたかどうか
+    /// </summary>
+    private bool isEvenFrameRendered = false;
+    private Vector4 worldPosition = new Vector4(0, 0, 0, -1);
+    // [SerializeField]
+    // private Renderer buffer1 = null;
+    // [SerializeField]
+    // private Renderer buffer2 = null;
 
     void Awake()
     {
-        Renderer renderer = GetComponent<Renderer>();
-        Texture mainTexture = renderer.material.mainTexture;
+        Texture mainTexture = myRenderer.material.mainTexture;
 
-        // RenderTextureの生成
-        paintTexture = new RenderTexture(mainTexture.width, mainTexture.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Default);
-        paintTexture.enableRandomWrite = true;
+        // 前フレームのテクスチャ参照用
+        paintBuffer1 = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+        paintBuffer2 = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+        paintBuffer1.enableRandomWrite = true;
+        paintBuffer2.enableRandomWrite = true;
 
-        // 元々のテクスチャを生成したRenderTextureにコピー
-        Graphics.Blit(mainTexture, paintTexture);
-
-        // paintMaterialのテクスチャに設定
-        int mainTextureId = Shader.PropertyToID("_MainTex");
-        paintMaterial.SetTexture(mainTextureId, paintTexture);
-
-        // MainTextureを生成したRenderTextureに変更する
-        renderer.material.mainTexture = paintTexture;
+        // メインテクスチャで初期化
+        Graphics.Blit(mainTexture, paintBuffer1);
+        Graphics.Blit(mainTexture, paintBuffer2);
 
         // 各シェーダープロパティIDを取得
         paintWorldPositionId = Shader.PropertyToID("_PaintWorldPosition");
-        viewProjectionInverseId = Shader.PropertyToID("_ViewProjectionInverse");
+        prevTexId = Shader.PropertyToID("_PrevTex");
+        currentTexId = Shader.PropertyToID("_CurrentTex");
 
-        // シェーダーでdepthを取り出すための設定
-        Camera.main.depthTextureMode = DepthTextureMode.Depth;
+        StartCoroutine(UpdateTextureAction());
+
+        // // for debug
+        // buffer1.material.mainTexture = paintBuffer1;
+        // buffer2.material.mainTexture = paintBuffer2;
     }
 
     void Update()
     {
-        Paint();
+        // Updateメソッドはレンダリング前に実行されるため，必要なデータはここでセットする．
+        UpdatePaint();
     }
 
-    // void OnRenderImage(RenderTexture src, RenderTexture dest)
-    // {
-    //     paintMaterial.SetVector(paintWorldPositionId, new Vector4(0, 0, 0, 0));
-
-    //     // from https://gamedev.stackexchange.com/questions/131978/shader-reconstructing-position-from-depth-in-vr-through-projection-matrix/140924#140924
-    //     Matrix4x4 projectionMatrix = GL.GetGPUProjectionMatrix(Camera.main.projectionMatrix, false);
-    //     projectionMatrix[2, 3] = projectionMatrix[3, 2] = 0.0f;
-    //     projectionMatrix[3, 3] = 1.0f;
-
-    //     Matrix4x4 clipToWorld = Matrix4x4.Inverse(projectionMatrix * Camera.main.worldToCameraMatrix) * Matrix4x4.TRS(new Vector3(0, 0, -projectionMatrix[2, 2]), Quaternion.identity, Vector3.one);
-    //     paintMaterial.SetMatrix(viewProjectionInverseId, clipToWorld);
-
-    //     // ペイント処理
-    //     Graphics.Blit(src, dest, paintMaterial);
-    // }
-
-    void Paint()
+    public void Paint(Vector3 worldPosition)
     {
-        paintMaterial.SetVector(paintWorldPositionId, new Vector4(0, 0, 0, 0));
+        this.worldPosition = new Vector4(worldPosition.x, worldPosition.y, worldPosition.z, 1);
+    }
 
-        // from https://gamedev.stackexchange.com/questions/131978/shader-reconstructing-position-from-depth-in-vr-through-projection-matrix/140924#140924
-        Matrix4x4 projectionMatrix = GL.GetGPUProjectionMatrix(Camera.main.projectionMatrix, false);
-        projectionMatrix[2, 3] = projectionMatrix[3, 2] = 0.0f;
-        projectionMatrix[3, 3] = 1.0f;
+    /// <summary>
+    /// リアルタイムペイントを行う．
+    /// </summary>
+    void UpdatePaint()
+    {
+        // シェーダーにペイントするワールド座標を設定する
+        myRenderer.material.SetVector(paintWorldPositionId, worldPosition);
 
-        Matrix4x4 clipToWorld = Matrix4x4.Inverse(projectionMatrix * Camera.main.worldToCameraMatrix) * Matrix4x4.TRS(new Vector3(0, 0, -projectionMatrix[2, 2]), Quaternion.identity, Vector3.one);
-        paintMaterial.SetMatrix(viewProjectionInverseId, clipToWorld);
+        if (Time.frameCount % 2 == 0)
+        {
+            // 偶数フレームはpaintBuffer1を保存用，paintBuffer2を書き込み用にする
+            myRenderer.material.SetTexture(prevTexId, paintBuffer1);
+            myRenderer.material.SetTexture(currentTexId, paintBuffer2);
+            isEvenFrameRendered = true;
 
-        // ペイント処理
-        RenderTexture tmp = RenderTexture.GetTemporary(paintTexture.width, paintTexture.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Default);  // 一時的なテクスチャを作成する
-        Graphics.Blit(paintTexture, tmp, paintMaterial);  // paintMaterialを基にtmpに描画する
-        Graphics.Blit(tmp, paintTexture); // その後，tmpに描画された結果をpaintTextureにコピー
-        RenderTexture.ReleaseTemporary(tmp);  // tmpを解放
+            // RWTexture2Dに書き込みを行うために必要
+            Graphics.ClearRandomWriteTargets();
+            Graphics.SetRandomWriteTarget(1, paintBuffer2);
+        }
+        else
+        {
+            // 奇数フレームはpaintBuffer2を保存用，paintBuffer1を書き込み用にする
+            myRenderer.material.SetTexture(prevTexId, paintBuffer2);
+            myRenderer.material.SetTexture(currentTexId, paintBuffer1);
+            isEvenFrameRendered = false;
+
+            // RWTexture2Dに書き込みを行うために必要
+            Graphics.ClearRandomWriteTargets();
+            Graphics.SetRandomWriteTarget(1, paintBuffer1);
+        }
+    }
+
+    /// <summary>
+    /// テクスチャを更新する処理
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator UpdateTextureAction()
+    {
+        while (true)
+        {
+            // スクリーン上のレンダリングが完了するまで待つ
+            yield return new WaitForEndOfFrame();
+
+            if (isEvenFrameRendered)
+            {
+                // paintBuffer2に入っている新しい値でpaintBuffer1を更新する．
+                Graphics.Blit(paintBuffer2, paintBuffer1);
+            }
+            else
+            {
+                Graphics.Blit(paintBuffer1, paintBuffer2);
+            }
+
+            // 次フレームではペイントしないようにするための処理
+            worldPosition.w = -1;
+        }
     }
 }
--- Assets/Shaders/RealTimePaint.shader
+++ Assets/Shaders/RealTimePaint.shader
@@ -2,8 +2,9 @@
 {
     Properties
     {
-        _MainTex ("Texture", 2D) = "white" {}   // ペイント用テクスチャ
-        _PaintColor ("PaintColor", Color) = (0,0,0,0)   // ペイントする色
+        _MainTex ("MainTexture", 2D) = "white" {}   // メインのテクスチャ
+        _BrushTex ("BrushTexture", 2D) = "white" {}   // ペイント用テクスチャ
+        _PaintColor ("PaintColor", Color) = (1,1,1,1)  // ペイントする色
         _BrushRadius ("BrushRadius", Float) = 5 // ペイントする範囲（半径）
     }
     SubShader
@@ -14,10 +15,9 @@
         Pass
         {
             CGPROGRAM
+            #pragma target 5.0
             #pragma vertex vert
             #pragma fragment frag
-            // make fog work
-            #pragma multi_compile_fog
 
             #include "UnityCG.cginc"
 
@@ -30,67 +30,96 @@
             struct v2f
             {
                 float2 uv : TEXCOORD0;
-                UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
-                float4 worldPos : TEXCOORD2;
-                float3 worldDirection : TEXCOORD3;
+                float3 worldPos : TEXCOORD1;
             };
 
             sampler2D _MainTex;
             float4 _MainTex_ST;
-            fixed4 _PaintColor;
+            sampler2D _BrushTex;
+            float4 _BrushTex_ST;
+            float4 _PaintColor;
             float _BrushRadius;
-            float4 _PaintWorldPosition; // ペイント中心位置
-            sampler2D _CameraDepthTexture;
-            float4 _CameraDepthTexture_ST;
-            float4x4 _ViewProjectionInverse; // (projction * view).inverse
+            float4 _PaintWorldPosition;
+            RWTexture2D<float4> _CurrentTex;    // register(u1); 更新後のテクスチャ保存用
+            Texture2D<float4> _PrevTex; // 前フレームのテクスチャ
+            float4 _PrevTex_TexelSize;  // _PrevTexのサイズ
+
+            /// 描画範囲に含まれているか
+            int isRange(float3 worldPos)
+            {
+                fixed dist = distance(_PaintWorldPosition.xyz, worldPos);
+
+                // if (_BrushRadius >= dist) then 1.0, else 0.0
+                return step(dist, _BrushRadius);
+            }
+
+            /// どの程度離れているか
+            float howFar(float3 worldPos)
+            {
+                fixed dist = distance(_PaintWorldPosition.xyz, worldPos);
+                fixed ratio = dist / _BrushRadius;
+
+                return saturate(ratio); // 0~1にクランプ
+            }
+
+            /// どの程度離れているかを2次元座標で示す(xy平面が基準)
+            float howFar2(float3 worldPos)
+            {
+                float far = howFar(worldPos);
+                fixed3 diff = worldPos - _PaintWorldPosition.xyz;
+
+                // xy平面の成分
+                half2 elem = dot(half2(1,1), diff);
+                return far * sign(elem);
+            }
 
             v2f vert (appdata v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = v.uv;
-                // o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
 
-                // TODO: ワールド座標を求める
+                // ワールド座標を求める
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
 
-                // float4 clip = float4((o.uv.x) * 2 - 1, (o.uv.y) * 2 - 1, 0, 1);
-
-                // MEMO: RenderTextureだとテクスチャ座標になる
-                float4 clip = float4(o.vertex.xy, 0, 1);
-                o.worldDirection = mul(_ViewProjectionInverse, clip).xyz - _WorldSpaceCameraPos;
-
-                UNITY_TRANSFER_FOG(o,o.vertex);
                 return o;
             }
 
+            // TODO: 微妙に色塗りの境界線が見えてしまう
             fixed4 frag (v2f i) : SV_Target
-            {
-                float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
-                depth = LinearEyeDepth(depth);
+            {   
+                // 参照するindexを取得
+                uint2 index = i.uv * _PrevTex_TexelSize.zw;
 
-                // float4 H = float4((i.uv.x) * 2 - 1, (i.uv.y) * 2 - 1, depth, 1);
-                float4 H = float4((i.uv.x) * 2 - 1, (i.uv.y) * 2 - 1, 0, 1);
+                // ペイント点からどれだけ近いかを[-1,1]の範囲で取得
+                float2 nearDegree = howFar2(i.worldPos);
 
-                float4 D = mul(_ViewProjectionInverse, H);
-                D = D / D.w;
-                
-                float3 worldPos = D.xyz + _WorldSpaceCameraPos;
-                return float4(i.worldDirection, 1);
-                return fixed4(i.worldDirection * depth + _WorldSpaceCameraPos, 1);
+                // brushTex用のUV座標を計算する
+                float2 brushUV = (nearDegree + float2(1,1)) / 2.0;
+                brushUV = TRANSFORM_TEX(brushUV, _BrushTex);
 
-                float2 uv = TRANSFORM_TEX(i.uv, _MainTex);
-                fixed4 col = tex2D(_MainTex, uv);
+                // ペイント色を取得
+                fixed4 brushColor = tex2D(_BrushTex, brushUV);
+                brushColor = brushColor * _PaintColor;
 
-                if (distance(worldPos.xyz, _PaintWorldPosition.xyz) < _BrushRadius) {
-                    col = _PaintColor * tex2D(_MainTex, uv);
+                // 新しい色でテクスチャを塗り替えるか判定
+                // _PaintWorldPosition.wが0以上の場合にペイント
+                int canPaint = lerp(0, isRange(i.worldPos), step(0, _PaintWorldPosition.w));
+
+                // 塗り替える場合は新しい色を返す
+                if (canPaint == 1) {
+                    half alpha = brushColor.w;
+                    fixed4 col = (1 - alpha) * _PrevTex[index] + alpha * brushColor;
+
+                    // 新しい値を保存
+                    _CurrentTex[index] = col;
+
+                    return col;
                 }
 
-                // // sample the texture
-                // fixed4 col = tex2D(_MainTex, i.uv);
-                // apply fog
-                UNITY_APPLY_FOG(i.fogCoord, col);
-                return col;
+                // 塗り替えない場合は前フレームの色をそのまま返す
+                return _PrevTex[index];
             }
             ENDCG
         }
